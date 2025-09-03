6a5fc1b6c04329a7ecca5cbd89f99f0228f7bc05
Omwami-dev <barasaowabarasa@gmail.com>
2025-09-03 21:00:57 +0300

    add inline styling

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,9 +1,13 @@
-function MainContent() {
+const MainContent = () => {
   return (
-    <main>
-      <p>I love to visit New York, Paris, and Tokyo.</p>
+    <main style={{ padding: '20px', textAlign: 'center', backgroundColor: '#eef2f3' }}>
+      <p style={{ fontSize: '18px', color: '#333' }}>
+        I love to visit <span style={{ color: 'darkred' }}>New York</span>, 
+        <span style={{ color: 'darkblue' }}> Paris</span>, and 
+        <span style={{ color: 'darkgreen' }}> Tokyo</span>.
+      </p>
     </main>
   );
-}
+};
 
 export default MainContent;
