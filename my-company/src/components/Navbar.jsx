a3c16caf73132da52f69a3a37b9225e42b55d40f
Omwami-dev <barasaowabarasa@gmail.com>
2025-09-03 22:09:05 +0300

    update my-company

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav
+      style={{
+        backgroundColor: "#333",
+        padding: "10px",
+      }}
+    >
+      <Link to="/" style={{ margin: "0 15px", color: "white" }}>
+        Home
+      </Link>
+      <Link to="/about" style={{ margin: "0 15px", color: "white" }}>
+        About
+      </Link>
+      <Link to="/services" style={{ margin: "0 15px", color: "white" }}>
+        Services
+      </Link>
+      <Link to="/contact" style={{ margin: "0 15px", color: "white" }}>
+        Contact
+      </Link>
+    </nav>
+  );
+}
+
+export default Navbar;
