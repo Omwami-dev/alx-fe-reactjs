581aa4ced65d2d08ba725f6259f66ba893e95d78
Omwami-dev <barasaowabarasa@gmail.com>
2025-09-03 20:47:11 +0300

    add inline styling

--- alx-react-app/src/components/Header.jsx
+++ alx-react-app/src/components/Header.jsx
@@ -1,9 +1,16 @@
-function Header() {
+const Header = () => {
   return (
-    <header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>>
-    <h1>My Favorite Cities</h1>
+    <header
+      style={{
+        backgroundColor: 'navy',
+        color: 'white',
+        textAlign: 'center',
+        padding: '15px'
+      }}
+    >
+      <h1>My Favorite Cities</h1>
     </header>
   );
-}
+};
 
 export default Header;
