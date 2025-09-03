cb5ae2a4a1f4a703bc2b4a2233e8cdc88e7c3088
Omwami-dev <barasaowabarasa@gmail.com>
2025-09-03 21:04:12 +0300

    update main

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -2,9 +2,7 @@ const MainContent = () => {
   return (
     <main style={{ padding: '20px', textAlign: 'center', backgroundColor: '#eef2f3' }}>
       <p style={{ fontSize: '18px', color: '#333' }}>
-        I love to visit <span style={{ color: 'darkred' }}>New York</span>, 
-        <span style={{ color: 'darkblue' }}> Paris</span>, and 
-        <span style={{ color: 'darkgreen' }}> Tokyo</span>.
+        I love to visit New York,Paris and Tokyo.
       </p>
     </main>
   );
