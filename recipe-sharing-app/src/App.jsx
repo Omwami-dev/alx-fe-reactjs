7d6ba381e2da0307298edc0ab0f701ada679ecf3
Omwami-dev <barasaowabarasa@gmail.com>
2025-09-12 13:58:20 +0300

    update app

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,29 +1,37 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeList from "./components/RecipeList";
+import RecipeDetails from "./components/RecipeDetails";
+import EditRecipeForm from "./components/EditRecipeForm";
 
 function Home() {
   return (
     <div>
-      <h2>Home Page</h2>
-      <p>
-        <Link to="/recipes/1">Go to Recipe 1</Link>
-      </p>
+      <AddRecipeForm />
+      <RecipeList />
     </div>
   );
-}
-
-function RecipeDetails() {
-  return <h2>Recipe Details Page</h2>;
 }
 
 export default function App() {
   return (
     <BrowserRouter>
-      <h1>🍲 Recipe Sharing App</h1>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/recipes/:id" element={<RecipeDetails />} />
-      </Routes>
+      <div style={{ maxWidth: 800, margin: "0 auto", padding: 20 }}>
+        <header style={{ marginBottom: 18 }}>
+          <h1>🍲 Recipe Sharing App</h1>
+          <nav>
+            <Link to="/">Home</Link>
+          </nav>
+        </header>
+
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/recipes/:id" element={<RecipeDetails />} />
+            <Route path="/recipes/:id/edit" element={<EditRecipeForm />} />
+          </Routes>
+        </main>
+      </div>
     </BrowserRouter>
   );
 }
-
