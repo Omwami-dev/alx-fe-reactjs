9079c863504aa2613514763d657fed7f8dd37cb1
Omwami-dev <barasaowabarasa@gmail.com>
2025-09-12 14:40:05 +0300

    update app

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,40 +1,40 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import AddRecipeForm from "./components/AddRecipeForm";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
 import RecipeDetails from "./components/RecipeDetails";
 import EditRecipeForm from "./components/EditRecipeForm";
 import SearchBar from "./components/SearchBar";
+import FavoritesList from "./components/FavoritesList";
+import RecommendationsList from "./components/RecommendationsList";
 
-
-function Home() {
+function App() {
   return (
-    <div>
-      <AddRecipeForm />
-      <SearchBar />
-      <RecipeList />
-    </div>
-  );
-}
+    <Router>
+      <div style={{ padding: "1rem" }}>
+        <nav style={{ marginBottom: "1rem", display: "flex", gap: "1rem" }}>
+          <Link to="/">Home</Link>
+          <Link to="/favorites">Favorites</Link>
+          <Link to="/recommendations">Recommendations</Link>
+        </nav>
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <div style={{ maxWidth: 800, margin: "0 auto", padding: 20 }}>
-        <header style={{ marginBottom: 18 }}>
-          <h1>🍲 Recipe Sharing App</h1>
-          <nav>
-            <Link to="/">Home</Link>
-          </nav>
-        </header>
-
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/recipes/:id" element={<RecipeDetails />} />
-            <Route path="/recipes/:id/edit" element={<EditRecipeForm />} />
-          </Routes>
-        </main>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <SearchBar />
+                <RecipeList />
+              </>
+            }
+          />
+          <Route path="/recipes/:id" element={<RecipeDetails />} />
+          <Route path="/recipes/:id/edit" element={<EditRecipeForm />} />
+          <Route path="/favorites" element={<FavoritesList />} />
+          <Route path="/recommendations" element={<RecommendationsList />} />
+        </Routes>
       </div>
-    </BrowserRouter>
+    </Router>
   );
 }
+
+export default App;
+
