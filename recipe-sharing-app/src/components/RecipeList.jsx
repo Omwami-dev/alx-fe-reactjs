02e98d08f5115992b31a8ea652b33082f7f1a0c5
Omwami-dev <barasaowabarasa@gmail.com>
2025-09-12 13:34:00 +0300

    update recipe list

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,42 +1,23 @@
-import { Link } from 'react-router-dom';
-import { useRecipeStore } from '../store/recipeStore';
-import DeleteRecipeButton from './DeleteRecipeButton';
+import { Link } from "react-router-dom";
+import { useRecipeStore } from "../store/recipeStore";
 
 const RecipeList = () => {
-  const recipes = useRecipeStore((s) => s.recipes);
+  const recipes = useRecipeStore((state) => state.recipes);
 
   return (
     <div>
-      <h2>Recipes</h2>
-      {recipes.length === 0 ? (
-        <p>No recipes yet. Add one!</p>
-      ) : (
-        recipes.map((recipe) => (
-          <div
-            key={recipe.id}
-            style={{
-              border: '1px solid #ddd',
-              padding: 12,
-              marginBottom: 12,
-              borderRadius: 6,
-            }}
-          >
-            <h3 style={{ margin: 0 }}>
-              <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
-            </h3>
-            <p style={{ marginTop: 6 }}>{recipe.description}</p>
-            <div style={{ marginTop: 8 }}>
-              <Link to={`/recipes/${recipe.id}/edit`} style={{ marginRight: 12 }}>
-                Edit
-              </Link>
-              <DeleteRecipeButton recipeId={recipe.id} />
-            </div>
-          </div>
-        ))
-      )}
+      {recipes.map((recipe) => (
+        <div key={recipe.id} style={{ marginBottom: "1rem" }}>
+          <h3>
+            <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
+          </h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
     </div>
   );
 };
 
 export default RecipeList;
+
 
