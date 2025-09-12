4fdf1efcffe770a692fad31901f59067fc9660b9
Omwami-dev <barasaowabarasa@gmail.com>
2025-09-12 13:35:05 +0300

    update recipe details

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,40 +1,24 @@
-import { Link, useParams } from 'react-router-dom';
-import { useRecipeStore } from '../store/recipeStore';
-import DeleteRecipeButton from './DeleteRecipeButton';
+import { useParams, Link } from "react-router-dom";
+import { useRecipeStore } from "../store/recipeStore";
 
 const RecipeDetails = () => {
   const { id } = useParams();
-  const recipeId = Number(id);
-  const recipe = useRecipeStore((s) =>
-    s.recipes.find((r) => r.id === recipeId)
+  const recipe = useRecipeStore((state) =>
+    state.recipes.find((r) => r.id === Number(id))
   );
 
   if (!recipe) {
-    return (
-      <div>
-        <h2>Recipe not found</h2>
-        <Link to="/">Back to list</Link>
-      </div>
-    );
+    return <p>Recipe not found ❌</p>;
   }
 
   return (
     <div>
-      <h2>{recipe.title}</h2>
+      <h1>{recipe.title}</h1>
       <p>{recipe.description}</p>
-
-      <div style={{ marginTop: 12 }}>
-        <Link to={`/recipes/${recipeId}/edit`} style={{ marginRight: 12 }}>
-          Edit
-        </Link>
-        <DeleteRecipeButton recipeId={recipeId} />
-      </div>
-
-      <div style={{ marginTop: 18 }}>
-        <Link to="/">Back to list</Link>
-      </div>
+      <Link to={`/recipes/${recipe.id}/edit`}>Edit</Link>
     </div>
   );
 };
 
 export default RecipeDetails;
+
