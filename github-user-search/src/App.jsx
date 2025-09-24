9eb1b123848407bba3369cfa16be364220c80ff4
Omwami-dev <barasaowabarasa@gmail.com>
2025-09-24 18:56:19 +0300

    update app.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 import React from "react";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div>
-      <h1>GitHub User Search</h1>
-      <p>Start by searching for a GitHub user...</p>
+      <h1 style={{ textAlign: "center" }}>GitHub User Search</h1>
+      <Search />
     </div>
   );
 }
 
 export default App;
+
